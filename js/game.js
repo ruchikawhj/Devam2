b15771ccbc0a5ef97b0980f615b0c1b92eb25a70
ruchikawhj <74497291+ruchikawhj@users.noreply.github.com>
2023-01-19 20:01:47 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,11 +2,11 @@ class Game {
   constructor() {
     this.resetTitle = createElement("h2");
     this.resetButton = createButton("");
-
+    this.leftKeyActive = false;
   }
 
   getState() {
-    database.ref("gameState").on("value", function(data) {
+    database.ref("gameState").on("value", function (data) {
       gameState = data.val();
     });
   }
@@ -33,19 +33,19 @@ class Game {
     jet2.scale = 1;
 
     jets = [jet1, jet2];
-    bulletGroup=new Group();
+    bulletGroup = new Group();
   }
 
   handleElements() {
     form.hide();
-   // form.titleImg.position(40, 50);
-   // form.titleImg.class("gameTitleAfterEffect");
-   this.resetTitle.html("Reset Game");
-   this.resetTitle.class("resetText");
-   this.resetTitle.position(width / 2 + 450, 40);
+    // form.titleImg.position(40, 50);
+    // form.titleImg.class("gameTitleAfterEffect");
+    this.resetTitle.html("Reset Game");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width / 2 + 450, 40);
 
-   this.resetButton.class("resetButton");
-   this.resetButton.position(width / 2 + 480, 100);
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 480, 100);
   }
 
   play() {
@@ -55,63 +55,73 @@ class Game {
 
     if (allPlayers !== undefined) {
       image(bg, 0, -height * 5, width, height * 6);
-      var index=0;
-      for(var plr in allPlayers){
-        index=index+1;
-        var x=allPlayers[plr].positionX;
-        var y=height-allPlayers[plr].positionY;
+      this.showLife();
+      var index = 0;
+      for (var plr in allPlayers) {
+        index = index + 1;
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
 
-      jets[index-1].position.x=x;
-    jets[index-1].position.y=y;
+        jets[index - 1].position.x = x;
+        jets[index - 1].position.y = y;
+        if (index === player.index) {
+
+          this.handlebulletCollision(index);
+
+        }
       }
       this.handlePlayerControls();
       drawSprites();
     }
   }
 
-  handlePlayerControls(){
-    if(keyDown(UP_ARROW)){
-      player.positionY+=10;
+  handlePlayerControls() {
+    if (keyDown(UP_ARROW)) {
+      player.positionY += 10;
       player.update();
+      this.leftKeyActive = true;
     }
-    if(keyDown(DOWN_ARROW)){
-      player.positionY-=10;
+    if (keyDown(DOWN_ARROW)) {
+      player.positionY -= 10;
       player.update();
+      this.leftKeyActive = true;
     }
-    if(keyDown(LEFT_ARROW)){
-      player.positionX-=10;
+    if (keyDown(LEFT_ARROW)) {
+      player.positionX -= 10;
       player.update();
+      this.leftKeyActive = true;
     }
-    if(keyDown(RIGHT_ARROW)){
-      player.positionX+=10;
+    if (keyDown(RIGHT_ARROW)) {
+      player.positionX += 10;
       player.update();
+      this.leftKeyActive = true;
     }
-    if(keyDown("space")){
+    if (keyDown("space")) {
       this.shoot();
     }
 
   }
-  shoot(){
-    if(player.index==1){
-      var bullet=createSprite(player.positionX,player.positionY)
-      bullet.x=player.positionX
-      bullet.y=height-player.positionY
-      bullet.velocityX=20;
+  shoot() {
+    if (player.index == 1) {
+      var bullet = createSprite(player.positionX, player.positionY)
+      bullet.x = player.positionX+50;
+      bullet.y = height - player.positionY
+      bullet.velocityX = 20;
       //bullet.depth=player.depth-2;
-      bullet.scale=0.2
+      bullet.scale = 0.2
       bullet.addImage(bullet1Img);
-      bullet.lifetime=800;
+      bullet.lifetime = 800;
       bulletGroup.add(bullet);
     }
-    else if(player.index==2){
-      var bullet=createSprite(player.positionX,player.positionY)
-      bullet.x=player.positionX
-      bullet.y=height-player.positionY
-      bullet.velocityX=-20;
+    else if (player.index == 2) {
+      var bullet = createSprite(player.positionX, player.positionY)
+      bullet.x = player.positionX-50
+      bullet.y = height - player.positionY
+      bullet.velocityX = -20;
       //bullet.depth=player.depth-2;
-      bullet.scale=0.2
+      bullet.scale = 0.2
       bullet.addImage(bullet2Img);
-      bullet.lifetime=800;
+      bullet.lifetime = 800;
       bulletGroup.add(bullet);
     }
   }
@@ -121,9 +131,35 @@ class Game {
         playerCount: 0,
         gameState: 0,
         players: {},
-     
+
       });
       window.location.reload();
     });
+  }
+
+  handlebulletCollision(index) {
+
+    if (jets[index - 1].collide(bulletGroup)) {
+      if (player.life > 0) {
+        bulletGroup.destroyEach();
+        player.life-=185/4;
+        
+      }
+      player.update();
+    }
+
+
+
+  }
+
+  showLife() {
+    push();
+    image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
+    fill("white");
+    rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
+    fill("#f50057");
+    rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
+    noStroke();
+    pop();
   }
 }
